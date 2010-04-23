22c4d1866c3a4d7f9361d4f020e4bb0e09374831
Nicholas J Humfrey <njh@aelius.com>
2010-04-23 23:15:53 +0100

    Implemented inserting/deleting triples - works with RedStore version 0.4.

--- lib/rdf/redstore/repository.rb
+++ lib/rdf/redstore/repository.rb
@@ -22,57 +22,94 @@ module RDF::RedStore
     # @example
     #     RDF::RedStore::Repository.new('http://localhost:8080/')
     #
-    def initialize(url, options = {})
-      @url      = url
-      @settings = options.dup
-      super(@url+'query', @settings)
+    def initialize(options_or_uri = {})
+      case options_or_uri
+        when Hash
+          @settings = options_or_uri.dup
+          @uri = @settings[:uri].to_s
+        else
+          @settings = {}
+          @uri = options_or_uri.to_s
+      end
+      super(uri_for(:query), @settings)
     end
 
     # @private
     def count
-      url = RDF::URI.parse(@url+'description')
-      description = RDF::Graph.load(url, :format => :ntriples)
-      result = description.query(:predicate => SD.totalTriples)
-      unless result.empty?
-        result.first.object.value.to_i
+      uri = uri_for(:description)
+      description = RDF::Graph.load(uri, :format => :ntriples)
+      total = description.first_value(:predicate => SD.totalTriples)
+      if total.nil? or total.to_i < 0
+        nil
+      else
+        total.to_i
       end
     end
     alias_method :size, :count
     alias_method :length, :count
 
     # @private
-    def insert_statement(statement)
-      raise NotImplementedError
+    def insert_statement(statement, opts = {})
+      insert_statements([statement], opts)
     end
 
     # @private
     def insert_statements(statements, opts = {})
-      raise NotImplementedError
+      p "insert_statements(statements, #{opts})"
+      post_statements(:insert, statements, opts)
     end
 
     # @private
-    def delete_statement(statement)
+    def delete_statement(statement, opts = {})
       if statement.invalid?
-        delete_statements(query(statement))
+        delete_statements(query(statement), opts)
       else
-        raise NotImplementedError
+        delete_statements([statement], opts)
       end
     end
 
     # @private
     def delete_statements(statements, opts = {})
-      return true if statements.empty?
-      raise NotImplementedError
+      post_statements(:delete, statements, opts)
     end
 
     # @private
     def clear_statements
-      raise NotImplementedError
+      uri = uri_for(:data)
+      Net::HTTP.start(uri.host, uri.port) do |http|
+        http.delete(uri.path).code == '200'
+      end
     end
 
     def writable?
       true
     end
 
+    # @private
+    def uri_for(action)
+      RDF::URI.parse(@uri+action.to_s)
+    end
+    
+    # @private
+    def post_statements(action, statements, opts = {})
+      return true if statements.empty?
+      graph = RDF::Graph.new
+      graph.insert_statements(statements)
+      content = RDF::Writer.for(:ntriples).dump(graph)
+
+      uri = uri_for(action)
+      req = Net::HTTP::Post.new(uri.path)
+      req.form_data = {
+        'content' => content,
+        'content-type' => 'ntriples',
+        'graph' => opts[:context].to_s
+      }
+
+      Net::HTTP.start(uri.host, uri.port) do |http|
+        response = http.request(req)
+        response.code == '200'
+      end
+    end
+    
   end
 end
